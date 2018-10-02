bed016c38600d6568d0c1615da0e8a673f6351a9
Ethan Ozelius <e.ozelius@gmail.com>
2018-10-02 16:18:20 -0400

    redux async actions and reducers working and stable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+import { selectSubreddit, fetchPostsIfNeeded } from './actions'
+import rootReducer from './reducers'
+
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware, // lets us dispatch() functions
+    loggerMiddleware // neat middleware that logs actions
+  )
+)
+store.dispatch(selectSubreddit('reactjs'))
+store.dispatch(fetchPostsIfNeeded('reactjs'))
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
